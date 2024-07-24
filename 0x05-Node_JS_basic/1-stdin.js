35e73872c6700985662fd3a918255e8cbba13475
iDamina <daminailiya@gmail.com>
2024-07-24 21:34:36 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,6 +7,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.pause();
+});
+
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
