562f02c56f7301a512dbb7a28d1716dd6a7119b2
iDamina <daminailiya@gmail.com>
2024-07-25 15:51:32 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,18 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
-  process.stdin.setEncoding('utf8');
-
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
+    const name = data.toString();
     process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
 } else {
-  process.stdin.setEncoding('utf8');
-
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
+    const name = data.toString();
     process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
-  process.stdin.on('end', () => {
+  process.on('end', () => {
     process.stdout.write('This important software is now closing\n');
   });
 }
