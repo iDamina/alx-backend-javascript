0a3ec515e8edebea9642f2d7bd2d2da801f9125c
iDamina <daminailiya@gmail.com>
2024-07-25 15:43:29 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,22 @@
-#!/usr/bin/env node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?\n');
+if (process.stdin.isTTY) {
+  process.stdin.setEncoding('utf8');
 
-process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
+  });
+} else {
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.pause();
-});
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
+  });
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
