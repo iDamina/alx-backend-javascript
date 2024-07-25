0a53121209ebaec3a6e394e09d0c1946e6fb8204
iDamina <daminailiya@gmail.com>
2024-07-25 16:41:19 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
+
 /**
  * This script prompts the user for their name and outputs a greeting.
  * When the input ends, it outputs a closing message.
  */
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
@@ -9,7 +12,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdin.pause(); // stop reading input
+  process.stdin.pause(); // Stop reading input
 });
 
 process.stdin.on('end', () => {
