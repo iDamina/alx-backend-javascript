06d369961b6c111d3c5b66d5ce2814bacf182c89
iDamina <daminailiya@gmail.com>
2024-07-27 04:00:36 +0100

    adjustments

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,16 +5,16 @@ const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const spy = sinon.spy(console);
+    const spy = sinon.spy(console, 'log');
     const dummy = sinon.stub(Utils, 'calculateNumber');
 
     dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
     expect(dummy.callCount).to.be.equal(1);
-    expect(spy.log.calledWith('The total is: 10')).to.be.true;
-    expect(spy.log.callCount).to.be.equal(1);
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+    expect(spy.callCount).to.be.equal(1);
     dummy.restore();
-    spy.log.restore();
+    spy.restore();
   });
 });
